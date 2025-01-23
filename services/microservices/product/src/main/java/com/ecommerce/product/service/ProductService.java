0dd88394a1065ac28cbac0a1467ffd869e8652e5
sai <laddusai520@gmail.com>
2025-01-23 11:15:19 +0530

    Redis implementation for Getting all products and Getting a product by its ID

--- services/microservices/product/src/main/java/com/ecommerce/product/service/ProductService.java
+++ services/microservices/product/src/main/java/com/ecommerce/product/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.ecommerce.product.service;
 
 import com.ecommerce.product.exception.ProductNotFoundException;
+import com.ecommerce.product.product.Product;
 import com.ecommerce.product.product.ProductDTO;
 import com.ecommerce.product.product.ProductMapper;
 import com.ecommerce.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,18 +14,34 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository repository;
     private final ProductMapper mapper;
+    private final RedisService redis;
 
     public List<ProductDTO> getAllProducts() {
-        return this.repository.findAll().stream()
+        Object obj = this.redis.get("products");
+        if(obj!=null){
+            log.info("Getting products from cache");
+            return (List<ProductDTO>)obj;
+        }
+        List<ProductDTO> products =  this.repository.findAll().stream()
                 .map(mapper::toPrductDTO)
                 .collect(Collectors.toList());
+        this.redis.set("products",products,300l);
+        return products;
     }
 
     public ProductDTO getProductById(String prodId) {
-        return this.repository.findById(prodId).map(mapper::toPrductDTO).orElseThrow(() -> new ProductNotFoundException("Product not found"));
+        Object obj = this.redis.get(prodId);
+        if(obj!=null) {
+            log.info("Getting product from cache");
+            return (ProductDTO)obj;
+        }
+        ProductDTO product = this.repository.findById(prodId).map(mapper::toPrductDTO).orElseThrow(() -> new ProductNotFoundException("Product not found"));
+        this.redis.set(prodId,product,300l);
+        return product;
     }
 }
