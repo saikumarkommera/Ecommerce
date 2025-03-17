13364095cf30ec91f90b4f54576267898d6d5586
sai <laddusai520@gmail.com>
2025-03-17 20:15:17 +0530

    apigateway request routing changes

--- services/microservices/customer/src/main/java/com/ecommerce/customer/controller/CustomerController.java
+++ services/microservices/customer/src/main/java/com/ecommerce/customer/controller/CustomerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/customer")
+@RequestMapping("/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
--- services/microservices/product/src/main/java/com/ecommerce/product/controller/ProductController.java
+++ services/microservices/product/src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/products")
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService service;
